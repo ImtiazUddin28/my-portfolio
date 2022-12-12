64c5d10a234b281533afb246a2b0d0ce1780d954
ImtiazUddin28 <imtiaz100463@gmail.com>
2022-12-12 20:52:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import {
+    Route,
+    Routes,
+} from "react-router-dom";
 import './App.css';
 
+/* Pages */
+import Home from "./pages/Home/HomePage";
+import About from "./pages/About/AboutPage";
+import Services from "./pages/Service/ServicesPage";
+import Project from "./pages/Project/ProjectPage";
+import ProjectApp from "./pages/Project/ProjectApp";
+import ProjectGame from "./pages/Project/ProjectGame";
+
+import RouterScrollTop from "./components/ScrollToTop/RouterScrollTop"
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 4000)
+    }, [])
+    return (
+        <>
+            <RouterScrollTop />
+            {
+                loading ?
+
+                    <div className='loading-pag'>
+                        <div className="loader">
+                            <span>=(imtiaz)=></span>
+                            <span>=(imtiaz)=></span>
+                        </div>
+                    </div>
+
+                    :
+
+                    <Routes>
+                        <Route path="/" element={<Home />}></Route>
+                        <Route exact path="/about" element={<About />}></Route>
+                        <Route exact path="/service" element={<Services />}></Route>
+                        <Route exact path="/project" element={<Project />}></Route>
+                        <Route exact path="/project/app" element={<ProjectApp />} />
+                        <Route exact path="/project/game" element={<ProjectGame />} />
+                    </Routes>
+
+            }
+
+        </>
+    )
 }
 
-export default App;
+export default App
